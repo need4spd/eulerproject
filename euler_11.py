fac87d744b0362b69a5e9e957a58e4f706c3cfd7
need4spd <need4spd@naver.com>
2012-02-06 19:14:31 +0900

    11번 문제 풀이

--- euler_11.py
+++ euler_11.py
@@ -1,3 +1,11 @@
+def multiplyOfElements(targetList):
+	result=1
+
+	for x in targetList:
+		result=result*int(x)
+	
+	return result
+		
 matrix=[]
 
 sampleFile=open("euler_11_prb.txt" ,"r")
@@ -8,17 +16,77 @@ for l in sampleFile.readlines():
 print(matrix)
 
 biggestNum=0
+biggestSlicedList=[]
 
 #first rule
-
-for rowindex in range(0,1):
+for rowindex in range(0,20):
 	selectedRow=matrix[rowindex]
 		
-	print(selectedRow)
-	for columnindex in range(4,20):
-		print(columnindex)
-		slicedList=selectedRow[columnindex:columnindex-4:-1]
+	for columnindex in range(4,21):
+		slicedList=selectedRow[columnindex-4:columnindex]
+	
+		r= multiplyOfElements(slicedList)
+	
+		if biggestNum < r:
+			biggestNum=r
+			biggestSlicedList=slicedList
+	
+
+print ("biggestNum : " + str(biggestNum))
+print ("slicedList : " + str(slicedList))
+
+#second rule
+for columnindex in range(0,20):
+	pivotList=[]
+
+	for rowindex in range(0,20):
+		pivotList.append(matrix[rowindex][columnindex])
 		
-		print(slicedList)
+		print("pivotList : " + str(pivotList))
 		
+	for columnindex in range(4,21):
+		slicedList= pivotList[columnindex-4:columnindex]
+	
+		r= multiplyOfElements(slicedList)
+	
+		if biggestNum < r:
+			biggestNum=r
+			biggestSlicedList=slicedList
+						
+print ("biggestNum : " + str(biggestNum))
+print ("slicedList : " + str(slicedList))		
+
+#third rule
+for rowLoop in range(0, 17):
+	for columnLoop in range(0, 17):
+		slicedList=[]
+		for arraySize in range(0, 4):
+			slicedList.append(matrix[arraySize + rowLoop][columnLoop + arraySize])
+
+		r = multiplyOfElements(slicedList)
+		
+		if biggestNum < r:
+			biggestNum=r
+			biggestSlicedList=slicedList
+						
+print ("biggestNum : " + str(biggestNum))
+print ("biggestSlicedList : " + str(biggestSlicedList))
+
+#fourth rule
+for rowLoop in range(0, 17):
+	for columnLoop in range(3, 19):
+		slicedList=[]
+		for arraySize in range(0, 4):
+			slicedList.append(matrix[arraySize + rowLoop][columnLoop - arraySize])
+			
+		r = multiplyOfElements(slicedList)
+		
+		if biggestNum < r:
+			biggestNum=r
+			biggestSlicedList=slicedList
+						
+print ("biggestNum : " + str(biggestNum))
+print ("biggestSlicedList : " + str(biggestSlicedList))
+
+
 		
