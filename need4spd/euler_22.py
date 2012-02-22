5a143c1dd812b980879284c02e8d0bbbe787e5fb
need4spd <need4spd@naver.com>
2012-02-22 14:40:04 +0900

    22번 풀이

---
+++ need4spd/euler_22.py
@@ -0,0 +1,45 @@
+#generate alphabet score
+#alphaList=map(chr, range(ord('A'), ord('Z')))
+alphaList = [chr(c) for c in range(ord('A'), ord('Z')+1)]
+
+print (alphaList)
+def getNameAndIndex(dataList):
+            
+    for index in range(0, len(dataList)):
+        name = dataList[index]
+        
+        print("name={0}, index={1}".format(name, index+1))
+        
+        yield name, index+1       
+
+def getNameScore(name):
+    sum=0    
+    for c in name:
+        sum=sum+alphaList.index(c)+1
+        print("c={0}, score={1}, namescore={2}".format(c, alphaList.index(c)+1, sum))        
+            
+    return sum
+
+totalScore=0
+indexOfName=0
+
+sampleFile=open("names.txt", "r")
+    
+names=sampleFile.readlines()
+namesStr=names[0]
+
+print("length : {0}".format(len(namesStr)))
+
+namesStr=namesStr.rstrip("\n")
+print("length : {0}".format(len(namesStr)))
+
+#print (namesStr.replace("\"","").split(","))
+nameList = namesStr.replace("\"","").split(",")
+nameList.sort()
+
+for name, index in getNameAndIndex(nameList):
+    (oldTotalScore, totalScore) = (totalScore, totalScore + (getNameScore(name) * (index)))
+    
+    print ("total score : {0}, minus : {1}".format(totalScore, totalScore-oldTotalScore))
+
+print ("total score : {0}".format(totalScore))
