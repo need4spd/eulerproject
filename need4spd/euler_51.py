ef916af607670e27b0aeb676d2de7d2ee9e7e6fb
need4spd2 <need4spd@naver.com>
2012-04-04 13:44:38 +0900

    solving 51

--- need4spd/euler_51.py
+++ need4spd/euler_51.py
@@ -11,19 +11,22 @@ def isPrime(n):
             return False
     return True
 
-max = 1000000
-min = 100000
-prime_list = [int(n) for n in range(min, max) if isPrime(n)]
-patterns = ['110001','101001','100101','100011','011001','010101','010011','001101','001011','000111']
-
+max = 100000
+min = 10000
+#prime_list = [int(n) for n in range(min, max) if isPrime(n)]
+#patterns = ['110001','101001','100101','100011','011001','010101','010011','001101','001011','000111']
+patterns = ['11001','10101','10011','10001','10111','11011','11101']
+prime_list = [56003]
 
 for p in prime_list:
   cnt = 0
+  replace_num_list = []
+  
   for i in range(0,10):
     for pat in patterns:
       replace_num = ""
-      
       p_index = 0
+
       for c in pat:
         if c == str(1):
           replace_num = replace_num + str(p)[p_index]
@@ -31,8 +34,14 @@ for p in prime_list:
           replace_num = replace_num + str(i)
         p_index += 1
       
-      if isPrime(replace_num) and int(replace_num) > 99999:
-        print (p, replace_num, int(replace_num), pat, i)
-        cnt += 1 
-  if cnt == 8:
-    print (p)
+      if not replace_num in replace_num_list:
+        replace_num_list.append(replace_num)
+  print (replace_num_list)
+      
+  for n in replace_num_list:
+    if isPrime(n) and int(n) > 9999:
+      print (p, n, int(n))
+      cnt += 1 
+  
+  if cnt == 6:
+    print ("result : ", p)
