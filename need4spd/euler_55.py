fb6d572e3338ff2add26c11f5111299c34fe222d
need4spd2 <need4spd@naver.com>
2012-04-17 13:02:59 +0900

    solving 55

---
+++ need4spd/euler_55.py
@@ -0,0 +1,32 @@
+max_loop_cnt = 50
+"""
+349 + 943 = 1292
+1292 + 2921 = 4213
+4213 + 3124 = 7337
+"""
+
+def isPalindrome(n):
+    n_str = str(n)
+    reverse_str = n_str[::-1]
+     
+    return n_str == reverse_str
+ 
+result=0
+loop_cnt = 0
+for n in range(349, 350):
+  while(True):
+    if loop_cnt == max_loop_cnt:
+      break
+    reverse_n = str(n)[::-1]
+    n = n +  int(reverse_n)
+    
+    print (n, reverse_n)
+    
+    if isPalindrome(n):
+      print("palindorem..."+str(n))
+      result += 1
+      break
+      
+    loop_cnt += 1
+
+print (10000 - result)
